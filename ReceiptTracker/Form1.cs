38d370d28af2d200daf5ecf0696960a8f64e0835
Dilip Muthukrishnan <cheepu82@gmail.com>
2014-04-01 00:59:41 -0400

    Finished most of the functionality

--- ReceiptTracker/Form1.cs
+++ ReceiptTracker/Form1.cs
@@ -12,10 +12,15 @@ namespace ReceiptTracker
 {
     public partial class Form1 : Form
     {
+        enum State
+        {
+            BLANK, ADD, DISPLAY, EDIT
+        }
 
         SqlConnection conn = null;
         SqlDataAdapter da = null;
         DataSet ds = null;
+        State state;
         int rowIndex = -1;
 
         public Form1()
@@ -25,15 +30,16 @@ namespace ReceiptTracker
 
         private void getData()
         {
-            string connStr = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\PROG37721\\ReceiptTracker\\ReceiptTracker\\receiptDB.mdf;Integrated Security=True;User Instance=True";
+            //string connStr = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\PROG37721\\ReceiptTracker\\ReceiptTracker\\receiptDB.mdf;Integrated Security=True;User Instance=True";
+            string connStr = "Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\dilip\\Desktop\\ReceiptTracker\\ReceiptTracker\\receiptDB.mdf;Integrated Security=True;User Instance=True";
             try
             {
                 conn = new SqlConnection(connStr);
                 conn.Open();
-                string sql = "SELECT * FROM [tReceipt]";
+                string sql = "SELECT * FROM [tReceipt] ORDER BY [date]";
                 da = new SqlDataAdapter(sql, conn);
                 //SqlCommandBuilder cb = new SqlCommandBuilder(da);
-                //createCommands();
+                createCommands();
                 ds = new DataSet();
                 da.Fill(ds, "tReceipt");
                 conn.Close();
@@ -42,6 +48,7 @@ namespace ReceiptTracker
                 bindingSource1.DataMember = "tReceipt";
                 dg1.DataSource = bindingSource1;
                 dg1.ClearSelection();
+                dg1.Columns["receiptID"].Visible = false;
             }
             catch (SqlException ex)
             {
@@ -55,39 +62,53 @@ namespace ReceiptTracker
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            dg1.Click += new EventHandler(dg1_Click);
             getData();
+            // Disable column sorting!
+            dg1.Columns["date"].SortMode = DataGridViewColumnSortMode.NotSortable;
+            dg1.Columns["description"].SortMode = DataGridViewColumnSortMode.NotSortable;
+            dg1.Columns["amount"].SortMode = DataGridViewColumnSortMode.NotSortable;
+            dg1.Columns["category"].SortMode = DataGridViewColumnSortMode.NotSortable;
+            dg1.Columns["tags"].SortMode = DataGridViewColumnSortMode.NotSortable;
+            dg1.Columns["type"].SortMode = DataGridViewColumnSortMode.NotSortable;
+            setControlState(State.BLANK);
+        }
 
-            SqlCommand cmd = new SqlCommand();
-            string sql = "INSERT INTO [tReceipt] ([date], [description],[amount]) VALUES (@date, @desc, @amount)";
-            cmd.Connection = conn;
-            cmd.CommandText = sql;
-            //create parameters
-            cmd.Parameters.Add("@date", SqlDbType.DateTime, 3, "Date");
-            cmd.Parameters.Add("@desc", SqlDbType.VarChar, 63, "Desc");
-            cmd.Parameters.Add("@amount", SqlDbType.Decimal, 9, "Amount");
-
-            da.InsertCommand = cmd;
-
-            DataRow dr = ds.Tables["tReceipt"].NewRow();
-            dr["Date"] = txtDate;
-            dr["Desc"] = "hello";
-            dr["Amount"] = 121.43;
-            ds.Tables["tReceipt"].Rows.Add(dr);
-            da.Update(ds, "tReceipt");
-
-            getData();
-
-
+        void dg1_Click(object sender, EventArgs e)
+        {
+            rowIndex = dg1.CurrentRow.Index;
+            /*// align rowIndex with index of selection in DataSet (very important!)
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                if (ds.Tables[0].Rows[i].RowState != DataRowState.Deleted)
+                {
+                    if (dg1.CurrentRow.Cells[0].Value.ToString().Equals(ds.Tables[0].Rows[i][0].ToString()))
+                    {
+                        rowIndex = i;
+                        break;
+                    }
+                }
+            }*/
+            txtDate.Value = DateTime.Parse(dg1.CurrentRow.Cells[1].Value.ToString());
+            txtDescription.Text = dg1.CurrentRow.Cells[2].Value.ToString();
+            txtAmount.Text = dg1.CurrentRow.Cells[3].Value.ToString();
+            txtCategory.Text = dg1.CurrentRow.Cells[4].Value.ToString();
+            txtTags.Text = dg1.CurrentRow.Cells[5].Value.ToString();
+            txtType.Text = dg1.CurrentRow.Cells[6].Value.ToString();
+            setControlState(State.DISPLAY);
         }
 
         private void cmdAddReceipt_Click(object sender, EventArgs e)
         {
-
+            setControlState(State.ADD);
         }
 
         private void cmdDeleteReceipt_Click(object sender, EventArgs e)
         {
-
+            ds.Tables[0].Rows[rowIndex].Delete();
+            da.Update(ds, "tReceipt");
+            getData();
+            setControlState(State.BLANK);
         }
 
         private void monthSelector_SelectedIndexChanged(object sender, EventArgs e)
@@ -97,17 +118,60 @@ namespace ReceiptTracker
 
         private void cmdSave_Click(object sender, EventArgs e)
         {
+            if (isDataGood())
+            {
+                DataRow dr = null;
+                int primaryKey = 0;
+                if (state == State.ADD)
+                {
+                    dr = ds.Tables["tReceipt"].NewRow();
+                }
+                else if (state == State.EDIT)
+                {
+                    dr = ds.Tables[0].Rows[rowIndex];
+                    primaryKey = (int)(dr["receiptID"]);
+                }
+                dr["Date"] = txtDate.Value.ToString("yyyy-MM-dd");
+                dr["Description"] = txtDescription.Text;
+                dr["Amount"] = Convert.ToDouble(txtAmount.Text);
+                dr["Category"] = txtCategory.Text;
+                dr["Tags"] = txtTags.Text;
+                dr["Type"] = txtType.Text;
+                if (state == State.ADD)
+                {
+                    ds.Tables["tReceipt"].Rows.Add(dr);
+                }
+                da.Update(ds, "tReceipt");
+                getData();
+                if (state == State.ADD)
+                {
+                    dg1.Rows[IndexOfRowWithLargestPrimaryKey()].Selected = true;
+                }
+                else if (state == State.EDIT)
+                {
+                    dg1.Rows[rowIndex = IndexOfRowWithPrimaryKey(primaryKey)].Selected = true;
 
+                }
+                setControlState(State.DISPLAY);
+            }
         }
 
         private void cmdCancel_Click(object sender, EventArgs e)
         {
-
+            if (state == State.EDIT)
+            {
+                setControlState(State.DISPLAY);
+            }
+            else
+            {
+                setControlState(State.BLANK);
+            }
+            
         }
 
         private void cmdEditReceipt_Click(object sender, EventArgs e)
         {
-
+            setControlState(State.EDIT);
         }
 
         private void cmdEditReceiptItems_Click(object sender, EventArgs e)
@@ -115,29 +179,16 @@ namespace ReceiptTracker
 
         }
 
-        private void txtDate_ValueChanged(object sender, EventArgs e)
+        private void clearText()
         {
-
-        }
-
-        private void txtDescription_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtAmount_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtCategory_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtTags_TextChanged(object sender, EventArgs e)
-        {
-
+            txtDate.Value = DateTime.Now;
+            txtDescription.Text = "";
+            txtAmount.Text = "";
+            txtCategory.Text = "";
+            txtType.Text = "Cash";
+            txtTags.Text = "";
+            dg1.ClearSelection();
+            txtDescription.Focus();
         }
 
         private bool isDataGood()
@@ -154,41 +205,169 @@ namespace ReceiptTracker
         {
             //create INSERT command for DataAdapter
             SqlCommand cmd = new SqlCommand();
-            string sql = "INSERT INTO [tClients] ([Account],[FirstName],[LastName],[Balance]) VALUES (@account, @firstname, @lastname, @balance)";
+            string sql = "INSERT INTO [tReceipt] ([date], [description],[amount],[category],[tags],[type]) VALUES (@date, @desc, @amount, @category, @tags, @type)";
             cmd.Connection = conn;
             cmd.CommandText = sql;
             //create parameters
-            cmd.Parameters.Add("@account", SqlDbType.Int, 4, "Account");
-            cmd.Parameters.Add("@firstname", SqlDbType.VarChar, 15, "FirstName");
-            cmd.Parameters.Add("@lastname", SqlDbType.VarChar, 15, "LastName");
-            cmd.Parameters.Add("@balance", SqlDbType.Float, 8, "Balance");
-            //add to DataAdapter
+            cmd.Parameters.Add("@date", SqlDbType.DateTime, 3, "Date");
+            cmd.Parameters.Add("@desc", SqlDbType.VarChar, 63, "Description");
+            cmd.Parameters.Add("@amount", SqlDbType.Decimal, 9, "Amount");
+            cmd.Parameters.Add("@category", SqlDbType.VarChar, 32, "Category");
+            cmd.Parameters.Add("@tags", SqlDbType.VarChar, 63, "Tags");
+            cmd.Parameters.Add("@type", SqlDbType.VarChar, 15, "Type");
+            // add to DataAdapter
             da.InsertCommand = cmd;
 
-            //create UPDATE command
+            //create  UPDATE command for DataAdapter
             cmd = new SqlCommand();
-            sql = "UPDATE [tClients] SET [Account] = @account, [FirstName] = @firstnamt, [LastName] = @lastname, [Balance] = @balance WHERE [Account] = @origAccount";
+            sql = "UPDATE [tReceipt] SET [date] = @date, [description] = @desc, [amount] = @amount, [category] = @category, [tags] = @tags, [type] = @type WHERE [receiptID] = @receiptID";
             cmd.Connection = conn;
             cmd.CommandText = sql;
             //create parameters
-            cmd.Parameters.Add("@account", SqlDbType.Int, 4, "Account");
-            cmd.Parameters.Add("@firstname", SqlDbType.VarChar, 15, "FirstName");
-            cmd.Parameters.Add("@lastname", SqlDbType.VarChar, 15, "LastName");
-            cmd.Parameters.Add("@balance", SqlDbType.Float, 8, "Balance");
-            SqlParameter param = cmd.Parameters.Add("@origAccount", SqlDbType.Int, 4, "Account");
-            param.SourceVersion = DataRowVersion.Original;
-            //add to DataAdapter
+            cmd.Parameters.Add("@receiptID", SqlDbType.Int, 4, "ReceiptID");
+            cmd.Parameters.Add("@date", SqlDbType.DateTime, 3, "Date");
+            cmd.Parameters.Add("@desc", SqlDbType.VarChar, 63, "Description");
+            cmd.Parameters.Add("@amount", SqlDbType.Decimal, 9, "Amount");
+            cmd.Parameters.Add("@category", SqlDbType.VarChar, 32, "Category");
+            cmd.Parameters.Add("@tags", SqlDbType.VarChar, 63, "Tags");
+            cmd.Parameters.Add("@type", SqlDbType.VarChar, 15, "Type");
+            // add to DataAdapter
             da.UpdateCommand = cmd;
 
             //create Delete command
             cmd = new SqlCommand();
-            sql = "DELETE FROM [tClients] WHERE [Account] = @origAccount";
+            sql = "DELETE FROM [tReceipt] WHERE [receiptID] = @receiptID";
             cmd.Connection = conn;
             cmd.CommandText = sql;
             //create parameter
-            cmd.Parameters.Add("@origAccount", SqlDbType.Int, 4, "Account").SourceVersion = DataRowVersion.Original;
+            cmd.Parameters.Add("@receiptID", SqlDbType.Int, 4, "ReceiptID");//.SourceVersion = DataRowVersion.Original;
             //add to DataAdapter
             da.DeleteCommand = cmd;
+        }
+
+        private int IndexOfRowWithLargestPrimaryKey()
+        {
+            // At this point, the data set should be updated with the latest row
+            int largestPrimaryKey = 0;
+            int index = 0;
+            int currentPrimaryKey = 0;
+            DataRow dr = null;
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                dr = ds.Tables[0].Rows[i];
+                currentPrimaryKey = (int)dr["receiptID"];
+                if (currentPrimaryKey > largestPrimaryKey)
+                {
+                    largestPrimaryKey = currentPrimaryKey;
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        private int IndexOfRowWithPrimaryKey(int primaryKey)
+        {
+            DataRow dr = null;
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                dr = ds.Tables[0].Rows[i];
+                if ((int)(dr["receiptID"]) == primaryKey)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        private void setControlState(State st)
+        {
+            switch (st)
+            {
+                case State.BLANK:
+                    clearText();
+                    txtDate.Enabled = false;
+                    txtAmount.Enabled = false;
+                    txtDescription.Enabled = false;
+                    txtCategory.Enabled = false;
+                    txtType.Enabled = false;
+                    txtTags.Enabled = false;
+                    cmdAddReceipt.Enabled = true;
+                    cmdSave.Enabled = false;
+                    cmdCancel.Enabled = false;
+                    cmdDeleteReceipt.Enabled = false;
+                    cmdEditReceipt.Enabled = false;
+                    cmdEditReceiptItems.Enabled = false;
+                    monthSelector.Enabled = false;
+                    cmdExportToFile.Enabled = false;
+                    break;
+                case State.ADD:
+                    txtDate.Enabled = true;
+                    txtAmount.Enabled = true;
+                    txtDescription.Enabled = true;
+                    txtCategory.Enabled = true;
+                    txtType.Enabled = true;
+                    txtTags.Enabled = true;
+                    cmdAddReceipt.Enabled = true;
+                    cmdSave.Enabled = true;
+                    cmdCancel.Enabled = true;
+                    cmdDeleteReceipt.Enabled = false;
+                    cmdEditReceipt.Enabled = false;
+                    cmdEditReceiptItems.Enabled = false;
+                    monthSelector.Enabled = false;
+                    cmdExportToFile.Enabled = false;
+                    //dg1.SelectionMode = DataGridViewSelectionMode.CellSelect;
+                    if (state == State.DISPLAY)
+                    {
+                        dg1.ClearSelection();
+                    }
+                    if (state == State.ADD)
+                    {
+                        clearText();
+                    }
+                    break;
+                case State.DISPLAY:
+                    txtDate.Enabled = false;
+                    txtAmount.Enabled = false;
+                    txtDescription.Enabled = false;
+                    txtCategory.Enabled = false;
+                    txtType.Enabled = false;
+                    txtTags.Enabled = false;
+                    cmdAddReceipt.Enabled = true;
+                    cmdSave.Enabled = false;
+                    cmdCancel.Enabled = false;
+                    cmdDeleteReceipt.Enabled = true;
+                    cmdEditReceipt.Enabled = true;
+                    cmdEditReceiptItems.Enabled = false;
+                    monthSelector.Enabled = false;
+                    cmdExportToFile.Enabled = false;
+                    break;
+                case State.EDIT:
+                    txtDate.Enabled = true;
+                    txtAmount.Enabled = true;
+                    txtDescription.Enabled = true;
+                    txtCategory.Enabled = true;
+                    txtType.Enabled = true;
+                    txtTags.Enabled = true;
+                    cmdAddReceipt.Enabled = false;
+                    cmdSave.Enabled = true;
+                    cmdCancel.Enabled = true;
+                    cmdDeleteReceipt.Enabled = true;
+                    cmdEditReceipt.Enabled = false;
+                    cmdEditReceiptItems.Enabled = false;
+                    monthSelector.Enabled = false;
+                    cmdExportToFile.Enabled = false;
+                    break;
+                default:
+                    break;
+            }
+            state = st;
+        }
+
+        private void dg1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            /*DataGridViewCell cell = dg1.CurrentRow.Cells[e.ColumnIndex];
+            DataGridViewColumn column = cell.OwningColumn;
+            Console.WriteLine(cell.Value.ToString());*/
         }
 
     }
